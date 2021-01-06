1e8bb7b71a28926fc1d568454b6220f125d54ba9
Shibida <shibida31@gmail.com>
2021-01-06 18:03:05 +0100

    feat(Ticker) : add missing attributes in Ticker object

--- src/main/java/com/cf/data/model/poloniex/PoloniexTicker.java
+++ src/main/java/com/cf/data/model/poloniex/PoloniexTicker.java
@@ -15,8 +15,11 @@ public class PoloniexTicker
     public final BigDecimal percentChange;
     public final BigDecimal baseVolume;
     public final BigDecimal quoteVolume;
+    public final BigDecimal isFrozen;
+    public final BigDecimal high24hr;
+    public final BigDecimal low24hr;
 
-    public PoloniexTicker(BigDecimal last, BigDecimal lowestAsk, BigDecimal highestBid, BigDecimal percentChange, BigDecimal baseVolume, BigDecimal quoteVolume)
+    public PoloniexTicker(BigDecimal last, BigDecimal lowestAsk, BigDecimal highestBid, BigDecimal percentChange, BigDecimal baseVolume, BigDecimal quoteVolume, BigDecimal isFrozen, BigDecimal high24hr, BigDecimal low24hr)
     {
         this.last = last;
         this.lowestAsk = lowestAsk;
@@ -24,6 +27,9 @@ public class PoloniexTicker
         this.percentChange = percentChange;
         this.baseVolume = baseVolume;
         this.quoteVolume = quoteVolume;
+        this.isFrozen = isFrozen;
+        this.high24hr = high24hr;
+        this.low24hr = low24hr;
     }
 
     @Override
